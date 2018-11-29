558b93966877ea2da5d3526925cb0a67ee3ab822
ecmerchant <ecmerchant.contact@gmail.com>
2018-11-30 02:19:16 +0900

    upd

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -48,6 +48,7 @@ class ItemsController < ApplicationController
                 "Accept-Encoding" => "gzip,deflate,br",
                 "Accept-Language" => "ja,en-US;q=0.9,en;q=0.8",
                 "Connection" => "keep-alive",
+                "Upgrade-Insecure-Requests" => "1",
                 "User-Agent" => "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36"
               }
               html = open(url, option) do |f|
